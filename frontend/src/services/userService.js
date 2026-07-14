e97d9004b7e7edde01fe07e1f549a5f490b6ac09
SergioFC89 <sergiofcu@ext.inditex.com>
2026-07-14 09:00:12 +0200

    [REF-001] Refactorizar manejo de API y eliminar clase Http

--- frontend/src/services/userService.js
+++ frontend/src/services/userService.js
@@ -1,34 +1,9 @@
-import { API_BASE_URL } from '@/config/api'
-import { createApiError } from '@/utils/apiErrors'
-import Http from './user'
+import { api } from './api-client'
 
 export async function getCurrentUser() {
-  const http = new Http(null, null, 'users/me')
-
-  try {
-    const data = await http.get()
-    return data
-  } catch (error) {
-    throw error
-  }
+  return api.get('users/me')
 }
+
 export async function updateCurrentUser(userData) {
-  const token = localStorage.getItem('access_token')
-
-  const respuesta = await fetch(`${API_BASE_URL}/users/edit_user`, {
-    method: 'PUT',
-    headers: {
-      Authorization: `Bearer ${token}`,
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(userData),
-  })
-
-  const data = await respuesta.json()
-
-  if (!respuesta.ok) {
-    throw createApiError(data, 'No se ha podido editar el perfil', respuesta.status)
-  }
-
-  return data
+  return api.put('users/edit_user', userData)
 }
